2728c11a1903274e1a5a5a5c190b79b118df6ae8
n1crack <yusuf@ozdemir.be>
2018-06-10 13:29:40 +0300

    better positioning on relative wrappers.

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ class mselect {
         this.selectbox.setAttribute('style', 'position:absolute;opacity: 0.25;' +
             'display:block;border: 1px solid deepskyblue;background: lightblue;z-index: 9999');
         this.container.appendChild(this.selectbox);
+        this.container.setAttribute('style', 'position:relative');
 
         window.addEventListener("mousemove", (this.calcSelection).bind(this));
         window.addEventListener("mouseup", this.stopSelection.bind(this));
@@ -44,8 +45,8 @@ class mselect {
 
     startSelection(e) {
         if (this.enabled) {
-            this.x1 = e.pageX;
-            this.y1 = e.pageY;
+            this.x1 = e.pageX - this.container.getBoundingClientRect().left - window.scrollX;
+            this.y1 = e.pageY - this.container.getBoundingClientRect().top - window.scrollY;
             if ((this.container.offsetLeft + this.container.clientWidth) > e.clientX) {
                 this.selection = true;
             }
@@ -60,46 +61,44 @@ class mselect {
     }
 
     calcSelection(e) {
-        // e.preventDefault();
         if (this.enabled && this.selection) {
 
+            let x = e.pageX - this.container.getBoundingClientRect().left - window.scrollX;
+            let y = e.pageY - this.container.getBoundingClientRect().top - window.scrollY;
 
-        let x = e.pageX;
-        let y = e.pageY;
+            this.box.top = Math.max(0, Math.min(y, this.y1));
+            this.box.left = Math.max(0, Math.min(x, this.x1));
 
-        this.box.top = Math.max(this.container.offsetTop, Math.min(y, this.y1));
-        this.box.left = Math.max(this.container.offsetLeft, Math.min(x, this.x1));
+            if (this.y1 < y) {
+                this.box.height = Math.min(this.container.offsetHeight - this.y1, y - this.y1);
+            } else {
+                this.box.height = Math.min(this.y1, this.y1 - y);
+            }
 
-        if (this.y1 < y) {
-            this.box.height = Math.min(this.container.offsetTop - this.y1 + this.container.offsetHeight, y - this.y1);
-        } else {
-            this.box.height = Math.min(this.y1 - this.container.offsetTop, this.y1 - y);
-        }
+            if (this.x1 < x) {
+                this.box.width = Math.min(this.container.offsetWidth - this.x1, x - this.x1);
+            } else {
+                this.box.width = Math.min(this.x1, this.x1 - x);
+            }
 
-        if (this.x1 < x) {
-            this.box.width = Math.min(this.container.offsetLeft - this.x1 + this.container.clientWidth, x - this.x1);
-        } else {
-            this.box.width = Math.min(this.x1 - this.container.offsetLeft, this.x1 - x);
-        }
+            Object.assign(this.selectbox.style, this.getPositionsInPixels());
 
-        Object.assign(this.selectbox.style, this.getPositionsInPixels());
+            if (typeof this.clear === "function") {
+                this.clear();
+            }
 
-        if (typeof this.clear === "function") {
-            this.clear();
-        }
-
-        this.container.querySelectorAll(this.nodes)
-            .forEach(function (item, index) {
-                if ((this.box.top < (item.offsetTop + item.offsetHeight - this.container.scrollTop))
-                    && ((this.box.top + this.box.height) > (item.offsetTop - this.container.scrollTop))
-                    && ((this.box.left) < (item.offsetLeft + item.offsetWidth - this.container.scrollLeft))
-                    && ((this.box.left + this.box.width) > (item.offsetLeft - this.container.scrollLeft))
-                ) {
-                    if (typeof this.select === "function") {
-                        this.select(item, index);
+            this.container.querySelectorAll(this.nodes)
+                .forEach(function (item, index) {
+                    if ((this.box.top < (item.offsetTop + item.offsetHeight - this.container.scrollTop))
+                        && ((this.box.top + this.box.height) > (item.offsetTop - this.container.scrollTop))
+                        && ((this.box.left) < (item.offsetLeft + item.offsetWidth - this.container.scrollLeft))
+                        && ((this.box.left + this.box.width) > (item.offsetLeft - this.container.scrollLeft))
+                    ) {
+                        if (typeof this.select === "function") {
+                            this.select(item, index);
+                        }
                     }
-                }
-            }.bind(this))
+                }.bind(this))
 
         }
     }
