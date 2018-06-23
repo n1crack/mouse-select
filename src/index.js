e73dc17e2b7d5ee9af837e37e89c6ebcce00a413
n1crack <yusuf@ozdemir.be>
2018-06-23 17:00:27 +0300

    selection starts only left click

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ class mselect {
     }
 
     startSelection(e) {
-        if (this.enabled) {
+        if (this.enabled && e.which === 1) {
             this.x1 = e.pageX - this.container.getBoundingClientRect().left - window.scrollX+ this.container.scrollLeft;
             this.y1 = e.pageY - this.container.getBoundingClientRect().top - window.scrollY+ this.container.scrollTop;
                 this.selection = true;
@@ -52,7 +52,7 @@ class mselect {
     }
 
     stopSelection(e) {
-        if (this.enabled) {
+        if (this.enabled && e.which === 1) {
             this.selection = false;
             Object.assign(this.selectbox.style, {top: 0, left: 0, width: 0, height: 0});
         }
